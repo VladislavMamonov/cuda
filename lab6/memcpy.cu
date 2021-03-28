b4d1a7f00d7923afd26a2824ba54ff4d15359f39
Vladislav Mamonov <vmwork0123@gmail.com>
2021-03-28 23:55:31 +0700

    2.0

--- lab6/memcpy.cu
+++ lab6/memcpy.cu
@@ -2,47 +2,22 @@
 
 
 #define N (1024 * 1024)
-#define FULL_DATA_SIZE (N * 20)
+#define FULL_DATA_SIZE (N * 10)
 
 
 using namespace std;
 
 
 
-#define CUDA_CHECK_RETURN(value) {\
-    cudaError_t _m_cudaStat = value;\
-    if (_m_cudaStat != cudaSuccess) {\
-        fprintf(stderr, "Error %s at line %d in file %s\n", cudaGetErrorString(_m_cudaStat), __LINE__, __FILE__);\
-        exit(1);\
-    }}
-
-
-
-__global__ void kernel(int *a, int *b, int *c)
+void memcpy()
 {
-    int idx = threadIdx.x + blockIdx.x * blockDim.x;
-    if (idx < N) {
-        int idx1 = (idx + 1) % 256;
-        int idx2 = (idx + 2) % 256;
-        float as = (a[idx] + a[idx1] + a[idx2]) / 3.0f;
-        float bs = (b[idx] + b[idx1] + b[idx2]) / 3.0f;
-        c[idx] = (as + bs) / 2;
-    }
-}
+    int *dev;
+    int *host = new int[FULL_DATA_SIZE];
 
+    cudaMalloc((void**)&dev, FULL_DATA_SIZE * sizeof(int));
 
-
-int main()
-{
-    int *dev_a, *dev_b, *dev_c;
-
-    int *host_a = new int[FULL_DATA_SIZE];
-    int *host_b = new int[FULL_DATA_SIZE];
-    int *host_c = new int[FULL_DATA_SIZE];
-
-    cudaMalloc((void**)&dev_a, FULL_DATA_SIZE * sizeof(int));
-    cudaMalloc((void**)&dev_b, FULL_DATA_SIZE * sizeof(int));
-    cudaMalloc((void**)&dev_c, FULL_DATA_SIZE * sizeof(int));
+    for (int i = 0; i < FULL_DATA_SIZE; i++)
+        host[i] = rand();
 
     float elapsedTime;
     cudaEvent_t start, stop;
@@ -50,16 +25,70 @@ int main()
     cudaEventCreate(&stop);
 
     cudaEventRecord(start, 0);
-    for (int i = 0; i < FULL_DATA_SIZE; i += N) {
-        cudaMemcpy(dev_a, host_a + i, N * sizeof(int), cudaMemcpyHostToDevice);
-        cudaMemcpy(dev_b, host_b + i, N * sizeof(int), cudaMemcpyHostToDevice);
-        kernel <<< N / 256, 256, 0 >>> (dev_a, dev_b, dev_c);
-        cudaMemcpy(host_c + i, dev_c, N * sizeof(int), cudaMemcpyDeviceToHost);
-    }
+    cudaMemcpy(dev, host, FULL_DATA_SIZE * sizeof(int), cudaMemcpyHostToDevice);
     cudaEventRecord(stop, 0);
     cudaEventSynchronize(stop);
-    CUDA_CHECK_RETURN(cudaDeviceSynchronize());
-    CUDA_CHECK_RETURN(cudaGetLastError());
     cudaEventElapsedTime(&elapsedTime, start, stop);
-    cout << "time: " << elapsedTime << " ms" << endl;
+    cout << "host->device elapsed time: " << elapsedTime << " ms" << endl;
+
+    cudaEventRecord(start, 0);
+    cudaMemcpy(host, dev, FULL_DATA_SIZE * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&elapsedTime, start, stop);
+    cout << "device->host elapsed time: " << elapsedTime << " ms" << endl;
+}
+
+
+
+void memcpy_PageLocked()
+{
+    int *dev;
+    int *host;
+
+    cudaMalloc((void**)&dev, FULL_DATA_SIZE * sizeof(int));
+    cudaHostAlloc((void**)&host, FULL_DATA_SIZE * sizeof(int), cudaHostAllocDefault);
+
+    for (int i = 0; i < FULL_DATA_SIZE; i++)
+        host[i] = rand();
+
+    float elapsedTime;
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    cudaEventRecord(start, 0);
+    cudaMemcpy(dev, host, FULL_DATA_SIZE * sizeof(int), cudaMemcpyHostToDevice);
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&elapsedTime, start, stop);
+    cout << "page-locked host->device elapsed time: " << elapsedTime << " ms" << endl;
+
+    cudaEventRecord(start, 0);
+    cudaMemcpy(host, dev, FULL_DATA_SIZE * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&elapsedTime, start, stop);
+    cout << "page-locked device->host elapsed time: " << elapsedTime << " ms" << endl;
+}
+
+
+
+int main()
+{
+    cudaDeviceProp prop;
+    int whichDevice;
+
+    cudaGetDevice(&whichDevice);
+    cudaGetDeviceProperties(&prop, whichDevice);
+    if (!prop.deviceOverlap) {
+        cout << "Device does not support overlapping" << endl;
+        return 1;
+    }
+
+    memcpy();
+    cout << endl;
+    memcpy_PageLocked();
+
+    return 0;
 }
